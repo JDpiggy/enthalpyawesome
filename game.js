2930c2b30a9a3fc20c758702947d5617a4188555
JDpiggy <jasoncpumpkin247@gmail.com>
2025-05-07 13:51:35 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
+const grassImg = new Image();
+grassImg.src = 'assets/tiles/grass.png';
 
 let gold = 100;
 let towers = [];
