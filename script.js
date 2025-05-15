6d77a126b372b3d97e357697b0d1d29ced735f67
JDpiggy <jasoncpumpkin247@gmail.com>
2025-05-15 10:31:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-FR8F4KWS11"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-FR8F4KWS11');
-</script>
 document.addEventListener('DOMContentLoaded', () => {
     // --- DOM ELEMENT GRABBING ---
     // ... (same as your previous version) ...
