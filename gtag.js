1dd1b65cc4f505c87d5d35969bf72288d17344a9
JDpiggy <jasoncpumpkin247@gmail.com>
2025-05-15 15:44:42 -0400

    Delete gtag.js

--- gtag.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-FR8F4KWS11"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-FR8F4KWS11');
-</script>
